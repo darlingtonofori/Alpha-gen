3bc9ec267d54b2bdc9af846a8e6dda8907cdd790
TR4N5P4R3NT <159647064+darlingtonofori@users.noreply.github.com>
2026-04-16 12:34:53 +0000

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 const activePairings = new Map();
 
 app.use(express.json());
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API: Request pairing code
